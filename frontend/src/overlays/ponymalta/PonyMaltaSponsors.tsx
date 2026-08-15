9350a533a6528d85aa8e5ace375a4177206043a4
Mauricio Sánchez <sanchezaguilarmauricio56@gmail.com>
2026-08-15 15:59:30 -0500

    Refactor PonyMaltaSponsors component and update CSS for improved layout
    - Changed sponsor alignment to center and adjusted padding for better spacing in the .pm-sponsors class.
    - Introduced a new class .pm-sponsor--mvsm for specific sponsor image styling, enhancing visual consistency.

--- frontend/src/overlays/ponymalta/PonyMaltaSponsors.tsx
+++ frontend/src/overlays/ponymalta/PonyMaltaSponsors.tsx
@@ -3,20 +3,8 @@ export function PonyMaltaSponsors() {
     <div className="pm-category">
       <div className="pm-sponsors">
         <img
-          className="pm-sponsor"
-          src="/overlays/ponymalta/sponsor-pony-malta.png"
-          alt=""
-          draggable={false}
-        />
-        <img
-          className="pm-sponsor"
-          src="/overlays/ponymalta/sponsor-alcaldia.png"
-          alt=""
-          draggable={false}
-        />
-        <img
-          className="pm-sponsor"
-          src="/overlays/ponymalta/sponsor-bavaria.png"
+          className="pm-sponsor pm-sponsor--mvsm"
+          src="/overlays/ponymalta/mvsm.png"
           alt=""
           draggable={false}
         />
